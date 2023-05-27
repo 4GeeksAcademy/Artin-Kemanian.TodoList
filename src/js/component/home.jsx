7149a020a7ff1d14d6d3bfbe19acd5f05a42a135
Artin-Kemanian <126287016+Artin-Kemanian@users.noreply.github.com>
2023-05-27 22:42:46 +0000

    primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// create your first component
 const Home = () => {
+	const [input1, setInput1] = useState("")
+	const [listelements, setListelements] = useState([])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="container container-fluid ">
+			<div className="header">
+				<h1 className="display-2 text-center">todos</h1>
+			</div>
+			<div className="d-flex justify-content-center">
+				<div className="card col-4">
+					<ul className="list-group list-group-flush">
+						<li className="list-group-item"><input className="border-0 w-100" type="text" onChange={(e) => setInput1(e.target.value)} value={input1} onKeyDown={(e) => {if (e.key === "Enter" && input1 != "") {
+						setListelements(listelements.concat(input1)); setInput1("")}
+						}} />
+						{listelements.map((item, index) => 
+							<li className="list-group-item">
+								<div className="row">
+									<div className="col-9">{item}</div>
+									<div className="col-3 d-flex justify-content-end"><a class="link-primary" onClick={() => setListelements(listelements.filter((t, currentIndex) => index != currentIndex))}>Borrar</a></div>
+								</div>
+							</li> )}
+						</li>
+					</ul>
+					<div className="card-footer">
+						<p className="numberOfTasks">{listelements.length} tareas</p>
+					</div>
+				</div>
+			</div>
 		</div>
+		</>
 	);
 };
 
